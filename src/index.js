eb7369ca5e626c00523ae30ea2a3328e2aa5c7f0
Ameya <joshiameyajoshi@gmail.com>
2023-11-29 00:49:36 -0600

    Broke down pages into different components

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import myimg from '../logo/react-logo.png'
 
+function Image() {
+    return <img src={myimg} alt="logo" width="40px" />
+}
 
-const page =  
-     (<div>
-        <img src={myimg} alt="logo" width="40px" />
-        <h1>Fun Facts About React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was Originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>);
+function Page() {
+     return(
+        <div>
+            <Image />
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was Originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+        );
+    };
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(page)
+root.render(<Page />)

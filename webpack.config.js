26e127057d723218b827e03a9797988eb95281f3
Ameya <joshiameyajoshi@gmail.com>
2023-11-29 00:43:02 -0600

    Initial commit for React project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin( {
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+            },
+            {               
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],                  
+            },
+        ]
+    },
+
+    devServer: {
+        port: 5000,
+        hot: true,
+        open: true,
+        liveReload: true,
+    },
+}

c549e46300f44cb1057efd67381c5c349ec0d9f8
Ameya <joshiameyajoshi@gmail.com>
2023-11-29 21:38:54 -0600

    Broke the page down into multiple components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import myimg from '../logo/react-logo.png'
+import myimg from '../logo/react-logo.png';
+
 
 function Image() {
     return <img src={myimg} alt="logo" width="40px" />
 }
 
-function Page() {
-     return(
-        <div>
+function Header() {
+    return ( 
+    <header>
+        <nav>
             <Image />
-            <h1>Fun Facts About React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was Originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </div>
-        );
-    };
+        </nav>
+    </header>);
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>"© 20xx Amjoe development. All rights reserved."</small>
+        </footer>
+    )
+}
+
+function Body() {
+    return (
+        <>
+            <h1>Reasons I am excited about React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </>
+    );
+}
+    
+function Page() {
+    return(
+       <div>
+           <Header />
+           <Body />
+           <Footer />
+       </div>
+       );
+   };
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<Page />)

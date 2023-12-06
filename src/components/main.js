c14107bb3b99591345df5d3b9c7c8414e8c3bbed
Ameya <joshiameyajoshi@gmail.com>
2023-12-06 00:58:50 -0600

    Added main body and its corresponding styling

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function Main() {
-    return (<h1>Main component goes here</h1>);
+    return (
+    <main>
+        <h1 class="main--title">Fun facts about React</h1>
+        <ul class="main--facts">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile</li>
+        </ul>
+    </main>
+    );
 }
